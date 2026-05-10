ff7cc713a91941304922c94144394093672a98da
Anggelo <arosalesdb23@gmail.com>
2026-05-10 16:23:38 -0600

    Add files via upload

---
+++ act3_lecc24_inciso1.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num, i, contador, suma;
+
+        suma = 0;
+
+        Console.WriteLine("Numeros primos entre 1 y 50:");
+        Console.WriteLine();
+
+        for (num = 1; num <= 50; num++)
+        {
+            contador = 0;
+
+            for (i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    contador = contador + 1;
+                }
+            }
+
+            if (contador == 2)
+            {
+                suma = suma + num;
+
+                if (num < 47)
+                {
+                    Console.Write(num + " + ");
+                }
+                else
+                {
+                    Console.Write(num);
+                }
+            }
+        }
+
+        Console.WriteLine(" = " + suma);
+
+        Console.ReadKey();
+    }
+}
---
+++ act3_lecc24_inciso2.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num, suma;
+
+        Console.WriteLine("Ingrese un numero:");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        suma = 0;
+
+        Console.WriteLine();
+        Console.WriteLine("Cuenta regresiva:");
+        Console.WriteLine();
+
+        while (num >= 1)
+        {
+            Console.WriteLine(num);
+
+            suma = suma + num;
+
+            num = num - 1;
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("La suma total es: " + suma);
+
+        Console.ReadKey();
+    }
+}
