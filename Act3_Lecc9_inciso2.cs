6b0acd6bc9cf651133e3e785dd7ebfdf583f2801
Anggelo <arosalesdb23@gmail.com>
2026-05-10 15:54:27 -0600

    Add files via upload

---
+++ Act3_Lecc9_inciso2.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num, i, contador;
+
+        Console.WriteLine("Ingrese un numero:");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        contador = 0;
+
+        for (i = 1; i <= num; i++)
+        {
+            if (num % i == 0)
+            {
+                contador = contador + 1;
+            }
+        }
+
+        if (contador == 2)
+        {
+            Console.WriteLine("Es numero primo");
+        }
+        else
+        {
+            Console.WriteLine("No es numero primo");
+        }
+
+        Console.ReadKey();
+    }
+}
